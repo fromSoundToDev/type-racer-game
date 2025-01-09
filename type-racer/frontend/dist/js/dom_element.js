faa08b307e0306b1d2bfc53fc7cdbc6abe545a24
fromSoundToDev <jeunejuste@gmail.com>
2025-01-09 14:50:00 +0000

    player finish name indicator

--- type-racer/frontend/dist/js/dom_element.js
+++ type-racer/frontend/dist/js/dom_element.js
@@ -9,6 +9,7 @@ const badgeCustomEL = document.querySelector('.badgeCustom');
 const lunchDescriptionEl = document.querySelector('.lunchDescription')
 const StartEl = document.querySelector('.start')
 const titlesEl = document.querySelector('.lunchCard-titles')
+const customBtnEl = document.querySelector('.lunchCard-btn')
 
 // functions that filled the card 
 
@@ -17,17 +18,31 @@ const showTrainCard = () => {
     lunchDescriptionEl.innerHTML = card_data.text[2]
     StartEl.innerHTML = card_data.btnContent 
     titlesEl.innerHTML = card_data.titles[0]
+    if (window.getComputedStyle(customBtnEl).display==='flex') {
+        customBtnEl.style.display='none'
+    } 
          
 }
 const showBotCard = () => {
     lunchDescriptionEl.innerHTML = card_data.text[1]
     StartEl.innerHTML = card_data.btnContent 
     titlesEl.innerHTML = card_data.titles[1]
+    if (window.getComputedStyle(customBtnEl).display==='flex') {
+        customBtnEl.style.display='none'
+    } 
 }
 const showCustomCard = () => {
     lunchDescriptionEl.innerHTML = card_data.text[0]
     StartEl.innerHTML = card_data.btnContent 
     titlesEl.innerHTML = card_data.titles[2]
+    
+
+
+    if (window.getComputedStyle(customBtnEl).display==='none') {
+        customBtnEl.style.display='flex'
+    } 
+        
+    
 }
 
 
